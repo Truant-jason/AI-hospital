d7d615231ac2a69f2e0ad71ee08a994f28ba124f
CS <cs@CSdeMacBook-Air.local>
2025-10-26 00:32:39 +0800

    初始化clinical_bot部分逻辑 -v0.0.1

--- baseAgent.py
+++ baseAgent.py
@@ -8,8 +8,13 @@
 '''
 
 from abc import ABC, abstractmethod 
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder  # 导入提示模板相关类
 import json
 import pymysql
+from langchain_openai import ChatOpenAI #后续考虑使用import openai
+from langchain_core.runnables.history import RunnableWithMessageHistory  # 导入带有消息历史的可运行类
+from langchain_core.messages import HumanMessage
+
 
 class BaseAgent(ABC):
     '''
@@ -42,10 +47,15 @@ class BaseAgent(ABC):
         初始化问诊机器人clinical_bot
 
     '''
-    def __init__(self, name, session_id, json_path, prompt_file,
+    def __init__(self, name, session_id, 
+                 llm_config,
+                 json_path, prompt_file,
                  mysql_host, mysql_user, mysql_password, mysql_db):
         self.name = name
         self.session_id = session_id
+
+
+        self.llm_config = llm_config    
 
         self.json_path = json_path
         self.prompt_file = prompt_file
@@ -70,6 +80,8 @@ class BaseAgent(ABC):
 
     def save_json(self):
         pass
+
+
 
     def visit_mysql(self):
         '''
@@ -105,6 +117,54 @@ class BaseAgent(ABC):
         except Exception as e:
             print(f"连接MySQL数据库失败: {e}")
             return False
+        
+    ## clinicalBot相关 ##
+    def create_clinicalBot(self):
+        '''
+        初始化问诊机器人
+        '''
+        system_prompt = ChatPromptTemplate.from_messages([
+            ("system", self.prompt_file),
+            MessagesPlaceholder(variable_name="history"),
+        ])
+
+        self.clinicalBot = system_prompt | ChatOpenAI(
+            model=self.llm_config['model'],
+            temperature=self.llm_config['temperature'],
+        )
+
+        self.clinicalBot_with_history = RunnableWithMessageHistory(
+            self.clinicalBot,
+            #TODO:待做功能 - get_session_history
+            self.get_session_history #可以直接使用langgraph的get_session_history方法或者自己写一个get_session_history方法
+        )
+
+    def chat_with_history(self, user_input, session_id=None):
+        '''
+        处理用户输入，生成包含聊天历史的回复
+        参数:
+            user_input (str): 用户输入的消息
+            session_id (str): 会话ID，默认为None
+        返回:
+            str: 聊天机器人的回复
+        '''
+        if session_id is None:
+            session_id = self.session_id
+        
+        response = self.clinicalBot_with_history.invoke(
+            [HumanMessage(content=user_input)],
+            {"configurable": {"session_id": session_id}}
+        )
+        return response.content
+    
+    @abstractmethod
+    def process_patient_data(self, patient_data):
+        '''
+        处理对应患者数据
+        '''
+        pass
+
+
 
 
 if __name__ == "__main__":
